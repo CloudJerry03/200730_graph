55ecfddb4d7b708103c8d5bbfe67f36ca491f397
CloudJerry03 <gomdalp1701@gmail.com>
2020-07-30 08:23:38 +0900

    진행중

---
+++ graph.js
@@ -0,0 +1,11 @@
+var left_is = document.getElementById("left");
+left_is.onclick = function() {
+    alter("짬뽕을 선택하셨습니다.")};
+    
+var left_bt= document.getElementById("left");
+left_bt.onclick = function() {
+    alert("left_click");};
+
+var right_bt= document.getElementById("right");
+right_bt.onclick = function() {
+    alert("right_click");};

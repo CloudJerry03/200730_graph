451447b58cc941a86e781709d98bfe3f8d1ed66f
CloudJerry03 <gomdalp1701@gmail.com>
2020-07-31 11:35:49 +0900

    아 윤영운 바보

--- graph.js
+++ graph.js
@@ -1,16 +1,15 @@
-var left_is = document.getElementById("left");
-left_is.addEventListener('click', function(){
-    alert("아아아아");
+let leftcount = 0;
+let rightcount = 0;
+
+let left_bt = document.getElementById("left_img");
+left_bt.addEventListener('click', function(){
+    leftcount += 1;
+    alert("짬뽕 " + leftcount + "번 선택하였습니다.");
 });
     
-let left_bt= document.getElementById("left");
-left_bt.addEventListener('click', function(){
-    alert("아아아아");
-});
 
-
-let day = document.getElementById('day');
-day.addEventListener('click', function () {
-    document.querySelector('body').style.backgroundColor = 'white';
-    document.querySelector('body').style.color = 'black';
+let right_bt = document.getElementById("right_img");
+right_bt.addEventListener('click', function(){
+    rightcount += 1;
+    alert("짜장면 " + rightcount + "번 선택하였습니다.");
 });
